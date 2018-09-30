b14732071d9ad4b76d37be574cc2a9d35c7ddbe2
Lucas Góes Valle <lgoesvalle@gmail.com>
2018-09-30 18:01:24 -0300

    create project

---
+++ SampleUnitTestsProject/SampleUnitTestsProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SampleUnitTestsProject
+//
+//  Created by Lucas Valle on 30/09/2018.
+//  Copyright © 2018 Lucas Valle. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
