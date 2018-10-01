4105a301213f26ccddd626ee73573dab92a504c8
Lucas Góes Valle <lgoesvalle@gmail.com>
2018-10-01 14:19:01 -0300

    new success case of test to validate in CI.

--- SampleUnitTestsProject/SampleUnitTestsProjectTests/SampleUnitTestsProjectTests.swift
+++ SampleUnitTestsProject/SampleUnitTestsProjectTests/SampleUnitTestsProjectTests.swift
@@ -20,15 +20,9 @@ class SampleUnitTestsProjectTests: XCTestCase {
     }
 
     func testExample() {
-        let jsonResult = ""
-        XCTAssertNotNil(jsonResult)
-    }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+        let name = "Lucas"
+        let anotherNmae = "Lucas"
+        XCTAssertEqual(name, anotherNmae)
     }
 
 }
